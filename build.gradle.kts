c73416304a348388437f2f7a30a63de4ccbc25b0
Hyune-c <choi8608@gmail.com>
2025-03-29 15:35:11 +0900

    project init

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dev.hyunec"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.jayway.jsonpath:json-path")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
+    val logstashLogbackVersion: String by project
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackVersion")
+
+    val kotlinLoggingVersion: String by project
+    implementation("io.github.oshai:kotlin-logging-jvm:$kotlinLoggingVersion")
+
+    val datafakerVersion: String by project
+    implementation("net.datafaker:datafaker:$datafakerVersion")
+
+    val kotestVersion: String by project
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/dev/hyunec/practiceparquet/PracticeParquetApplication.kt
@@ -0,0 +1,11 @@
+package dev.hyunec.practiceparquet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeParquetApplication
+
+fun main(args: Array<String>) {
+    runApplication<PracticeParquetApplication>(*args)
+}
